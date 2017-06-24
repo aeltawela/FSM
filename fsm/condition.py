ccab88444ca303bf77b4deff63e9afccca3f5909
Ahmed Eltawela <ahmedabt@gmail.com>
2017-06-24 12:59:26 +0200

    Added conditions helper functions (i.e. any, all, reverse) Increased the test coverage

--- fsm/condition.py
+++ fsm/condition.py
@@ -1,13 +1,29 @@
-def any(self):
-    pass
+def _any(*args):
+    for arg in args:
+        if arg():
+            return True
+
+    return False
 
 
-def all(self):
-    pass
+def any(*args):
+    return lambda: _any(*args)
 
 
-def reverse(condition):
-    return not condition
+def _all(*args):
+    for arg in args:
+        if not arg():
+            return False
+
+    return True
+
+
+def all(*args):
+    return lambda: _all(*args)
+
+
+def reverse(condition: callable):
+    return lambda: not condition()
 
 
 class Condition:
--- tests/test_fsm.py
+++ tests/test_fsm.py
@@ -3,6 +3,8 @@ from unittest.mock import MagicMock
 
 from fsm import Fsm, Transition, State
 
+_global_fsm = None
+
 
 class FsmTest(unittest.TestCase):
     @staticmethod
@@ -10,8 +12,17 @@ class FsmTest(unittest.TestCase):
         return True
 
     @staticmethod
+    def _condition_two():
+        return False
+
+    @staticmethod
     def _handler_one():
         pass
+
+    @staticmethod
+    def _handler_two():
+        global _global_fsm
+        _global_fsm.trigger_event("done")
 
     def test_state_transitions(self):
         fsm = Fsm(
@@ -23,7 +34,7 @@ class FsmTest(unittest.TestCase):
 
                 Transition(event="done", from_state=State("State 2"), to_state=State("State 3"),
                            condition=self._condition_one,
-                           transition_handler=self._handler_one)
+                           transition_handler=self._handler_one),
             ),
             initial_state=State("State 1"),
             logger=MagicMock())
@@ -32,6 +43,37 @@ class FsmTest(unittest.TestCase):
         self.assertEqual(fsm.current_state, State("State 2"))
         fsm.trigger_event("done")
         self.assertEqual(fsm.current_state, State("State 3"))
+
+    def test_internal_transition(self):
+        global _global_fsm
+
+        fsm = Fsm(
+            transitions=(
+                Transition(event="done", from_state=State("State 1"), to_state=State("State 2"),
+                           condition=self._condition_one,
+                           transition_handler=self._handler_one),
+
+                Transition(event="done", from_state=State("State 2"), to_state=State("State 3"),
+                           condition=self._condition_two,
+                           transition_handler=self._handler_one),
+
+                Transition(event="done", from_state=State("State 2"), to_state=State("State 4"),
+                           condition=self._condition_one,
+                           transition_handler=self._handler_two),
+
+                Transition(event="done", from_state=State("State 4"), to_state=State("State 5"),
+                           condition=self._condition_one,
+                           transition_handler=self._handler_one),
+            ),
+            initial_state=State("State 1"),
+            logger=MagicMock())
+
+        _global_fsm = fsm
+
+        fsm.trigger_event("done")
+        self.assertEqual(fsm.current_state, State("State 2"))
+        fsm.trigger_event("done")
+        self.assertEqual(fsm.current_state, State("State 5"))
 
 
 if __name__ == '__main__':
